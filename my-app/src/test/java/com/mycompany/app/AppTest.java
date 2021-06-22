a0e2561c70da46e72894f07626b6311bb2e7dd06
Roy <royyab@gmail.com>
2021-06-22 16:40:21 +0300

    amen

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -18,19 +18,19 @@ public class AppTest
      * Rigorous Test :-)
      */
 
-    // @Test
-    // public void shouldAnswerWithTrue()
-    // {
-    //     int a = 1;
-    //     int b = 2;
-    //     assertTrue(false);
-    // }
-    //
-    // @Test
-    // public void shouldAnswerWithFalse()
-    // {
-    //     int a = 1;
-    //     int b = 2;
-    //     assertTrue(false);
-    // }
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        int a = 1;
+        int b = 2;
+        assertTrue(false);
+    }
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        int a = 1;
+        int b = 2;
+        assertTrue(false);
+    }
 }

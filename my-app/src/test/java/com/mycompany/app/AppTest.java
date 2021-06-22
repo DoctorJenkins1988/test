c9d269c38dfe61aae2c480231a66711bad81ac28
Roy <royyab@gmail.com>
2021-06-22 16:28:01 +0300

    change workspace!

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -17,19 +17,20 @@ public class AppTest
      *
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        int a = 1;
-        int b = 2;
-        assertTrue(false);
-    }
 
-    @Test
-    public void shouldAnswerWithFalse()
-    {
-        int a = 1;
-        int b = 2;
-        assertTrue(false);
-    }
+    // @Test
+    // public void shouldAnswerWithTrue()
+    // {
+    //     int a = 1;
+    //     int b = 2;
+    //     assertTrue(false);
+    // }
+    //
+    // @Test
+    // public void shouldAnswerWithFalse()
+    // {
+    //     int a = 1;
+    //     int b = 2;
+    //     assertTrue(false);
+    // }
 }
